ebbc6e2eef6900b6bd1db3d90647f57fe7da5a8c
Eirik91 <55440585+Eirik91@users.noreply.github.com>
2020-01-30 08:56:28 +0100

    Eiirk

---
+++ Oblig1/Oblig1/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oblig1
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BirthYear { get; set; }
+        public Person Father { get; set; }
+        public Person Mother { get; set; }
+
+        public void Show() 
+        {
+            if (FirstName != null)
+            {
+                Console.WriteLine("Firstname: " + FirstName);
+            }
+            if (LastName != null)
+            {
+                Console.WriteLine("Lastname: " + LastName);
+            }
+            if (BirthYear != 0) 
+            {
+            Console.WriteLine("Birthyear: " + BirthYear);
+            }
+            if (Mother != null)
+            {
+                Console.WriteLine("Mother: " + Mother.FirstName);
+            } 
+            if (Father != null)
+            {
+                Console.WriteLine("Father: " + Father.FirstName);
+            }
+            if (Id != 0)
+            {
+                Console.WriteLine("Id: " + Id);
+            }
+        }
+    }
+}
---
+++ Oblig1/Oblig1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oblig1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Velkommen til det kongelige slektstre!");
+            var ListPeople = PersonList();
+          
+            while (true)
+            {
+                Console.Write("Skriv hva du ønsker å gjøre eller hjelp for hjelp: ");
+                
+                var input = Console.ReadLine();
+                if (input == "hjelp")
+                {
+                    ShowHelpText();
+                }
+                if (input == "liste")
+                {
+                    ShowAllPeople(ListPeople);
+                }
+                var splitArray = input.Split(' ');
+                if (splitArray[0] == "vis" && Convert.ToInt32(splitArray[1]) >= ListPeople.Count)
+                {
+                    Console.WriteLine($"Det er ingen med så høy id, den høyeste id'en er: {ListPeople.Count}");
+                    continue;
+                }
+                if (splitArray[0] == "vis" && Convert.ToInt32(splitArray[1]) == 0)
+                {
+                    Console.WriteLine("Det er ingen med id'en 0.");
+                    continue;
+                }
+                    if (splitArray[0] == "vis" && splitArray.Length > 1)
+                {
+                    var PersonId = Convert.ToInt32(splitArray[1]) - 1;
+                    ListPeople[PersonId].Show();
+                }
+                if (input != "hjelp" && input != "liste" && splitArray[0] != "vis") 
+                {
+                    Console.WriteLine("Du skrev noe feil. Skriv hjelp for kommandoer eller prøv på nytt");
+                }
+
+            }
+        }
+
+        private static void ShowAllPeople(List<Person> listPeople)
+        {
+            for (var i = 0; i < listPeople.Count; i++) 
+            {
+                listPeople[i].Show();
+            }
+        }
+
+        private static void ShowHelpText()
+        {
+            Console.WriteLine("Skriv liste for å få opp en liste av alle personer.");
+            Console.WriteLine("Skriv f.eks vis 3 (altså vis (id)) for å få opp vedkommende med mor og far.");
+            Console.WriteLine("ctrl+c for å avslutte ");
+        }
+        public static List<Person> PersonList()
+        {
+            var sverreMagnus = new Person { Id = 1, FirstName = "Sverre Magnus", BirthYear = 2005 };
+            var ingridAlexandra = new Person { Id = 2, FirstName = "Ingrid Alexandra", BirthYear = 2004 };
+            var haakon = new Person { Id = 3, FirstName = "Haakon Magnus", BirthYear = 1973 };
+            var metteMarit = new Person { Id = 4, FirstName = "Mette-Marit", BirthYear = 1973 };
+            var marius = new Person { Id = 5, FirstName = "Marius", LastName = "Borg Høiby", BirthYear = 1997 };
+            var harald = new Person { Id = 6, FirstName = "Harald", BirthYear = 1937 };
+            var sonja = new Person { Id = 7, FirstName = "Sonja", BirthYear = 1937 };
+            var olav = new Person { Id = 8, FirstName = "Olav", BirthYear = 1903 };
+
+            sverreMagnus.Father = haakon;
+            sverreMagnus.Mother = metteMarit;
+            ingridAlexandra.Father = haakon;
+            ingridAlexandra.Mother = metteMarit;
+            marius.Mother = metteMarit;
+            haakon.Father = harald;
+            haakon.Mother = sonja;
+            harald.Father = olav;
+
+            var list = new List<Person>
+            {
+            sverreMagnus, ingridAlexandra, haakon, metteMarit, marius, harald, sonja, olav
+            };
+        return list;
+        }
+    }
+}
